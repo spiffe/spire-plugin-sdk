b59b964f004ecdba7fa4f8daa9787229fad8b3a5
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2023-02-24 11:46:55 -0300

    Introduce the BundlePublisher interface (#38)
    * Introduce the BundlePublisher interface
    
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

---
+++ templates/server/bundlepublisher/bundlepublisher_test.go
@@ -0,0 +1,36 @@
+package bundlepublisher_test
+
+import (
+	"testing"
+
+	"github.com/spiffe/spire-plugin-sdk/pluginsdk"
+	"github.com/spiffe/spire-plugin-sdk/plugintest"
+	bundlepublisherv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/server/bundlepublisher/v1"
+	configv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/service/common/config/v1"
+	"github.com/spiffe/spire-plugin-sdk/templates/server/bundlepublisher"
+)
+
+func Test(t *testing.T) {
+	plugin := new(bundlepublisher.Plugin)
+	pluginClient := new(bundlepublisherv1.BundlePublisherPluginClient)
+	configClient := new(configv1.ConfigServiceClient)
+
+	// Serve the plugin in the background with the configured plugin and
+	// service servers. The servers will be cleaned up when the test finishes.
+	// TODO: Remove the config service server and client if no configuration
+	// is required.
+	// TODO: Provide host service server implementations if required by the
+	// plugin.
+	plugintest.ServeInBackground(t, plugintest.Config{
+		PluginServer: bundlepublisherv1.BundlePublisherPluginServer(plugin),
+		PluginClient: pluginClient,
+		ServiceServers: []pluginsdk.ServiceServer{
+			configv1.ConfigServiceServer(plugin),
+		},
+		ServiceClients: []pluginsdk.ServiceClient{
+			configClient,
+		},
+	})
+
+	// TODO: Invoke methods on the clients and assert the results
+}
